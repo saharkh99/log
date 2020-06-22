5022bb992f2866ddc656e3ec1d1db5329c1efa65
saharkh <skhazali99@gmail.com>
2020-06-22 20:36:28 +0430

    login api

---
+++ Mvc2/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mvc2.Models;
+using System.Web.Mvc;
+
+namespace Mvc2.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserEntities us = new UserEntities();
+       private static CodeGenerator cg1 = new CodeGenerator();
+
+       public void insert(user user) {
+
+
+           us.users.Add(user);
+           us.SaveChanges();
+                   
+       }
+
+        public String Login(user login)
+        {
+            using (UserEntities us = new UserEntities())
+            {
+
+                var lusername = us.users.Where(a => a.username == login.username).FirstOrDefault();
+                var lpassword = us.users.Where(a => a.passwords == login.passwords).FirstOrDefault();
+                if (lusername != null && lpassword != null)
+                {
+
+                    var jsonData = Json(login.username, JsonRequestBehavior.AllowGet);
+                    return "true";
+                }
+                else
+                {
+                    var jsonData = Json(login.username, JsonRequestBehavior.AllowGet);
+                    return "false";
+                }
+
+            }
+        
+        }
+   //     public String setCode() {
+         
+    //        cg1.code = cg1.generator();
+     //       return cg1.code;
+    //    }
+        
+     //   public String getCode(CodeGenerator cg)
+     //   {
+         
+     //       if (cg.code == cg1.code)
+     //           return "true";
+       //     else
+       //         return "false";
+            
+     //   }
+
+    }
+}
+
---
+++ Mvc2/Models/CodeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc2.Models
+{
+    public class CodeGenerator
+    {
+        public string code{set;get;}
+        
+        public string generator() {
+            const string Chars = "ABCDEFGHIJKLMNPOQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(Chars, 4)
+                    .Select(s => s[random.Next(s.Length)])
+                    .ToArray());
+            return result;
+
+        }
+        public void set(string result) {
+            code = result;
+        }
+        public string get() {
+            
+            return code;
+        
+        }
+
+    }
+}
