33aa9e290527719a5350e7822772e7d131dc7784
saharkh <skhazali99@gmail.com>
2020-06-28 21:50:17 +0430

    "crt token"

--- Mvc2/Controllers/HomeController.cs
+++ Mvc2/Controllers/HomeController.cs
@@ -7,16 +7,22 @@ using System.Web.Mvc;
 using System.Data;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Net;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Web.Http;
 
-
-namespace Mvc2.Controllers
+namespace WebApi.Jwt.Controllers
 {
-    
+
     public class HomeController : Controller
     {
-
+        private const string Secret = "db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==";
         Database1Entities us = new Database1Entities();
       // private static CodeGenerator cg1 = new CodeGenerator();
+         [AllowAnonymous]
         public String Login(user login)
         {
             using (Database1Entities us = new Database1Entities())
@@ -26,19 +32,18 @@ namespace Mvc2.Controllers
                 var lpassword = us.users.Where(a => a.passwords == login.passwords).FirstOrDefault();
                 if (lusername != null && lpassword != null)
                 {
-
-                    var jsonData = Json(login.username, JsonRequestBehavior.AllowGet);
-                    return "true";
+                   
+                    return JwtManager.GenerateToken(lusername.username);
                 }
                 else
                 {
-                    var jsonData = Json(login.username, JsonRequestBehavior.AllowGet);
                     return "false";
                 }
 
             }
         
         }
+        
        // public string insert() {
 
          //   user u = new user();
@@ -50,7 +55,13 @@ namespace Mvc2.Controllers
     //        cg1.code = cg1.generator();
      //       return cg1.code;
     //    }
-        
+       
+ 
+
+
+
+
+
      //   public String getCode(CodeGenerator cg)
      //   {
          
---
+++ Mvc2/JwtManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi.Jwt
+{
+    public static class JwtManager
+    {
+        private const string Secret = "db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==";
+
+        public static string GenerateToken(string username, int expireMinutes = 20)
+        {
+            var symmetricKey = Convert.FromBase64String(Secret);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, username)
+        }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(symmetricKey),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+    }
+}
